f1a25966353d23a3da61e72ecb225adaabf0fc13
oliyka <olja.x.06@gmail.com>
2025-06-21 11:02:13 +0300

    added base elem

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,3 +1,7 @@
 //Обʼєкт з посиланнями на ДОМ елементи
 
-export const refs = {};
+export const refs = {
+    categoryList: document.querySelector(".categories"),
+    productList: document.querySelector(".products"),
+    notFound: document.querySelector(".not-found"),
+};
