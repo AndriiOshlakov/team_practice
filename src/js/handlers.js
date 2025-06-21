3afdb80351db5f254e63132164dca6b0a7bf0314
Yana Khovbosha <aemec4373@gmail.com>
2025-06-21 12:54:34 +0300

    add handlers

--- src/js/handlers.js
+++ src/js/handlers.js
@@ -1,1 +1,22 @@
 // Функції, які передаються колбеками в addEventListners
+
+import {
+  markupCategories,
+  getsearchForm,
+  renderModalProductMarkup,
+} from './render-function';
+
+export function markupCategories(event) {
+  const btn = event.target.textContent;
+  console.log(btn);
+}
+
+export function getsearchForm(event) {
+  event.preventDefault();
+  const value = event.currentTarget.elements.search.value.trim();
+  console.log(value);
+}
+
+export function renderModalProductMarkup(event) {
+  onModal(id);
+}
