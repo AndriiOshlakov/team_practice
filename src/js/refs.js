37c98d55ea76acbaa8b9399577233b97cf8c7947
oliyka <olja.x.06@gmail.com>
2025-06-21 11:36:17 +0300

    added

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,21 +1,32 @@
 //Обʼєкт з посиланнями на ДОМ елементи
 
 export const refs = {
+
+    // Lists
+
     categoryList: document.querySelector(".categories"),
     productList: document.querySelector(".products"),
     cartList: document.querySelector(".cart-summary__list"),
 
+    // Not found
+
     notFound: document.querySelector(".not-found"),
+
+    // Search form
 
     searchForm: document.querySelector(".search-form"),
     searchFormInput: document.querySelector(".search-form__input"),
     searchFormClearBtn: document.querySelector(".search-form__btn-clear"),
     searchFormBtn: document.querySelector(".search-form__btn"),
 
+    // Navigation
+
     navList: document.querySelector(".nav__list"),
     homeLink: document.querySelector('.nav__link[href="./index.html"]'),
     cartLink: document.querySelector('.nav__link[href="./cart.html"]'),
     wishlistLink: document.querySelector('.nav__link[href="./wishlist.html"]'),
+
+    // Modal
 
     modalCloseBtn: document.querySelector(".modal__close-btn"),
     modalProd: document.querySelector(".modal-product"),
