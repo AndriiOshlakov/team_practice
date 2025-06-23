42ea1c25635258a405cd9a355862f1313d19e205
AndriiOshlakov <andreyoshlakov18051986@gmail.com>
2025-06-23 06:33:23 +0000

    Deploying to gh-pages from @ AndriiOshlakov/team_practice@4edb0c9006f4ab9eac062ff012db81c6998eabfb 🚀

---
+++ wishlist.js
@@ -0,0 +1,2 @@
+import{i as t,b as i}from"./assets/helpers-GpbVV26d.js";t();var e;(e=document.querySelector(".theme-btn"))==null||e.addEventListener("click",i);
+//# sourceMappingURL=wishlist.js.map
